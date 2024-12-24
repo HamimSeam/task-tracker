244df92c34822bde5949aef7ba7465177eec4b62
Hamim Seam <hamimhseam@gmail.com>
2024-12-24 14:35:01 -0500

    Move addTask logic to a popup modal

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,11 +1,12 @@
-import './TaskList.css'
+import "./TaskList.css";
 import { useState } from "react";
+import ReactDom from "react-dom";
 
 function Task({ task }) {
   const { title, course, dueDate, description, label } = task;
 
   return (
-    <div className='task'>
+    <div className="task">
       <h3>{title}</h3>
       <ul>
         <li>Course: {course}</li>
@@ -17,19 +18,19 @@ function Task({ task }) {
   );
 }
 
-function TaskList() {
-  const [tasks, setTasks] = useState([]);
+function TaskModal({ addTask, mode, onClose }) {
+  if (mode === "close") return null;
 
-  function addTask(event) {
+  function handleFormSubmit(event) {
     event.preventDefault();
     const form = event.target;
 
-    const title = form['title'].value;
-    const course = form['course'].value;
-    const dueDate = form['due-date'].value;
-    const description = form['description'].value;
-    const label = form['label'].value;
-    const id = Date.now()
+    const title = form["title"].value;
+    const course = form["course"].value;
+    const dueDate = form["due-date"].value;
+    const description = form["description"].value;
+    const label = form["label"].value;
+    const id = Date.now();
 
     const taskInfo = {
       title,
@@ -37,44 +38,72 @@ function TaskList() {
       dueDate,
       description,
       label,
-      id
+      id,
     };
 
-    console.log(taskInfo)
-    setTasks([...tasks, taskInfo]);
+    console.log(taskInfo);
+    addTask(taskInfo);
   }
+
+  return ReactDom.createPortal(
+    <>
+      <div className="task-modal-overlay" />
+      <div className="task-modal">
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="title">Task Title: </label>
+          <input type="text" id="title" name="title" required />
+          <br />
+
+          <label htmlFor="course">Course: </label>
+          <input type="text" id="course" name="course" />
+          <br />
+
+          <label htmlFor="due-date">Due Date: </label>
+          <input type="date" id="due-date" name="due-date" />
+          <br />
+
+          <label htmlFor="description">Description: </label>
+          <input type="text" id="description" name="description" />
+          <br />
+
+          <label htmlFor="label">Label: </label>
+          <input type="text" id="label" name="label" />
+          <br />
+
+          <button type="submit">Add Task</button>
+        </form>
+        <button type="button" onClick={onClose}>
+          Close Modal
+        </button>
+      </div>
+    </>,
+    document.getElementById("task-modal")
+  );
+}
+
+function TaskList() {
+  const [tasks, setTasks] = useState([]);
+  const [modalMode, setModalMode] = useState("close");
 
   return (
     <>
-      <form onSubmit={addTask}>
-        <label htmlFor="title">Task Title: </label>
-        <input type="text" id="title" name="title" required />
-        <br />
-
-        <label htmlFor="course">Course: </label>
-        <input type="text" id="course" name="course" />
-        <br />
-
-        <label htmlFor="due-date">Due Date: </label>
-        <input type="date" id="due-date" name="due-date" />
-        <br />
-
-        <label htmlFor="description">Description: </label>
-        <input type="text" id="description" name="description" />
-        <br />
-
-        <label htmlFor="label">Label: </label>
-        <input type="text" id="label" name="label" />
-        <br />
-
-        <button type="submit">
-          Add Task
-        </button>
-      </form>
+      <button type="button" onClick={() => setModalMode("open")}>
+        +
+      </button>
 
       {tasks.map((task, index) => (
         <Task key={index} task={task} />
       ))}
+      {
+        <TaskModal
+          addTask={(newTask) => {
+            setTasks([...tasks, newTask]);
+            setModalMode("close");
+          }}
+          mode={modalMode}
+          onClose={() => setModalMode("close")}
+        ></TaskModal>
+      }
     </>
   );
 }
